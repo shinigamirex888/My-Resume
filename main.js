0d5b70cea026c7b77a0b545637e899858c8f2299
Snehil <snehilsneh888@gmail.com>
2021-04-20 08:35:35 +0530

    some minor changes

--- main.js
+++ main.js
@@ -1,28 +1,100 @@
-var navMenuAnchorTags = document.querySelectorAll('.nav-menu a');
-var interval;
-
-
-for (var i = 0; i < navMenuAnchorTags.length; i++) {
-    navMenuAnchorTags[i].addEventListener('click', function (event) {
-        event.preventDefault();
-        var targetSectionID = this.textContent.trim().toLowerCase();
-        console.log(this.textContent);
-        var targetSection = document.getElementById(targetSectionID);
-        console.log(targetSection);
-        //    interval = setInterval(scrollVertically, 20, targetSection);
-
-        interval = setInterval(function () {
-            scrollVertically(targetSection);
-        }, 20);
-    });
+"use strict";
+/* code for smooth scroll */
+function add_listener_to_the_element(itr, amount_of_scroll)
+{
+    itr.addEventListener('click', function (event)
+    {
+        event.preventDefault();/* to prevent the default behaviour, i.e. sudden scrolling by default */
+        let position_of_element_from_top = document.getElementById(itr.innerText.toLowerCase()).getBoundingClientRect().y;
+        var id = setInterval(function ()
+        {
+            window.scrollBy(0, amount_of_scroll);/* here the second entry is the amount by which i want to scroll */
+            if (window.pageYOffset >= position_of_element_from_top) {
+                clearInterval(id);
+                return;
+            }
+        }, 1)/* this is the time after which it will scroll */
+    })
+}
+var list = document.getElementsByClassName("horizontal-list")[0].getElementsByTagName('li');
+for (let itr of list) {
+    if (itr.innerText == "Home") {
+        continue;
+    }
+    else if (itr.innerText == "About") {
+        add_listener_to_the_element(itr, 15);
+    }
+    else if (itr.innerText == "Skills") {
+        add_listener_to_the_element(itr, 15);
+    }
+    else if (itr.innerText == "Experience") {
+        add_listener_to_the_element(itr, 15);
+    }
+    else if (itr.innerText == "Education") {
+        add_listener_to_the_element(itr, 15);
+    }
+    else if (itr.innerText == "Portfolio") {
+        add_listener_to_the_element(itr, 25);
+    }
+    // else if (itr.innerText == "Contact") {
+    //     add_listener_to_the_element(itr, 20);
+    // }
 }
 
-
-function scrollVertically(targetSection) {
-    var targetSectionCoordinates = targetSection.getBoundingClientRect();
-    if (targetSectionCoordinates.top <= 0) {
-        clearInterval(interval);
-        return;
+var list_of_bars = document.querySelectorAll('.skill-progress>div');
+function set_width_to_zero()
+{
+    for (let bar of list_of_bars) {
+        bar.style.width = "0";
     }
-    window.scrollBy(0, 50);
-} 
+}
+function in_view(bar)
+{
+    let element_distance = bar.getBoundingClientRect().top;
+    if (element_distance < window.innerHeight) {
+        return true;
+    }
+    return false;
+}
+function fill_bar(bar, percentage)
+{
+    let count = 0;
+    let id = setInterval(function ()
+    {
+        bar.style.width = count.toString() + "%";
+        if (count++ == percentage) {
+            clearInterval(id);
+        }
+    }, 10)
+}
+function is_filled(bar)
+{
+    if (parseInt(bar.style.width) != 0) {
+        return true;
+    }
+    return false;
+}
+function checker_in_view()
+{
+    for (let bar of list_of_bars) {
+        if (in_view(bar) && !is_filled(bar)) {
+            fill_bar(bar, bar.getAttribute("data-skill-percent"));
+        }
+    }
+}
+function again_on_top()
+{
+    let skills = document.getElementById('skills')
+    let skills_distance = skills.getBoundingClientRect().top;
+    if (skills_distance > window.innerHeight) {
+        set_width_to_zero();
+    }
+}
+function listener()
+{
+    checker_in_view();
+    again_on_top()
+}
+
+set_width_to_zero();
+window.addEventListener('scroll', listener);
